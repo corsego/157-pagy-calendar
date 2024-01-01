1b70c118b4b31a7e9f9f22cfedd43540566e3b83
yshmarov <yshmarov@gmail.com>
2024-01-01 19:46:51 +0100

    scaffold events

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+path = "https://raw.githubusercontent.com/ruby-conferences/ruby-conferences.github.io/master/_data/conferences.yml"
+uri = URI.open(path)
+yaml = YAML.load_file uri, permitted_classes: [Date]
+
+yaml.each do |event|
+  Event.create!(
+    name: event["name"],
+    location: event["location"],
+    start_date: event["start_date"]
+  )
+end
