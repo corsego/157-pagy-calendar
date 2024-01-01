3219df0b6d1f9ad117c10f5e3212355a56633b42
yshmarov <yshmarov@gmail.com>
2024-01-01 23:26:26 +0100

    wow

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,7 +5,8 @@ class CalendarController < ApplicationController
   def week
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @events = Event.where(start_date: @date.beginning_of_week..@date.end_of_week)
-    @last_week_date = @date - 1.week
-    @next_week_date = @date + 1.week
+    # the params in the url should be 1 week back
+    @last_week_date = (@date - 1.week).beginning_of_week
+    @next_week_date = (@date + 1.week).beginning_of_week
   end
 end
