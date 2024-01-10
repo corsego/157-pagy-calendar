fad396e2afd4aec5785dedf9a83a09b891da5815
yshmarov <yshmarov@gmail.com>
2024-01-10 21:21:32 +0100

    clean up helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def days_of_week(date)
-    date.beginning_of_week..date.end_of_week
-  end
-
-  def days_of_month(date)
-    date.beginning_of_month..date.end_of_month
-  end
-
   def month_offset(date)
     date.beginning_of_month.wday - 1
   end
